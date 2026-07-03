1cf6f6536a8e61ba8e6be967027dcbe3dc84cc77
Mokshita kothari <mokshitakothari@gmail.com>
2026-07-03 15:39:10 +0530

    Delete src directory

--- src/db.ts
+++
@@ -1,9 +0,0 @@
-import { openDB } from "idb";
-
-export const dbPromise = openDB("shop-db", 1, {
-  upgrade(db) {
-    db.createObjectStore("products", { keyPath: "barcode" });
-    db.createObjectStore("inventory", { keyPath: "barcode" });
-    db.createObjectStore("sales", { autoIncrement: true });
-  },
-});
